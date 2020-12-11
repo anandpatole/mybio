302490d9352ec9a4f248807c94226b6f3180df08
androidmahindra <mobilityhelpdesk.mm@gmail.com>
2020-12-11 17:52:38 +0530

    Updates

---
+++ precache-manifest.59c8ff29bc28e2b4a47407c8678b95bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a09a9df3bb284fb0f5a2",
+    "url": "/mybio/static/css/main.c70d74a1.chunk.css"
+  },
+  {
+    "revision": "a09a9df3bb284fb0f5a2",
+    "url": "/mybio/static/js/main.a09a9df3.chunk.js"
+  },
+  {
+    "revision": "aaf2d35a6f1a57231d34",
+    "url": "/mybio/static/js/runtime~main.aaf2d35a.js"
+  },
+  {
+    "revision": "1cf4b44a3ecf5bc6e327",
+    "url": "/mybio/static/js/2.1cf4b44a.chunk.js"
+  },
+  {
+    "revision": "d6f7850067732ec10d4be2f78bb832ec",
+    "url": "/mybio/index.html"
+  }
+];
