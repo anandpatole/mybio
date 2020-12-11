7f01ef728dda232b5525afa625666399d00ea66f
androidmahindra <mobilityhelpdesk.mm@gmail.com>
2020-12-11 17:28:23 +0530

    react first commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      
+                       <h2 >Senior Android Developer at Mahindra & Mahindra Ltd <span> Dec 2018-present</span></h2> 
+                       <img alt="" src='images/neosoft.png' width='60' height='30'></img>
+                        <p>I joined NeoSoft technologies  as a Senior Android-Developer in the Mobility team. My major part of the work has been into the field of creating Android Application for Mahindra & Mahindra Client</p>
+                       
+                      </div>
+                    </div>
+                  
+                  </article>
+
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Senior Android Developer at Tops Grup <span>June 2018-Oct 2018</span></h2>
+                        <img alt="" src='images/top_grup.jpg' width='35' height='40'></img>
+                        <p>I joined NeoSoft technologies  as a Senior Android-Developer in the Mobility team. My major part of the work has been into the field of creating Android Application for Mahindra & Mahindra Client</p>
+                      </div>
+                    </div>
+                  </article> 
+                  {/* <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
